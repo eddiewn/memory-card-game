4ecc794401396d75bbddfda19fe3b891f0efc336
Eddie Wiik Nilsson <eddie.wiiknilsson@gmail.com>
2025-09-26 09:10:59 +0200

    Now shuffles the cardArray and prints random every time

--- src/components/CardGrid.tsx
+++ src/components/CardGrid.tsx
@@ -7,29 +7,44 @@ type CardType = {
 };
 
 const cardData: CardType[] = [
-    { id: 1, name: "Tiger", img: "/tiger.png" },
-    { id: 2, name: "Tiger", img: "/tiger.png" },
-    { id: 3, name: "Elephant", img: "/elephant.png" },
-    { id: 4, name: "Elephant", img: "/elephant.png" },
-    { id: 5, name: "Lion", img: "/lion.png" },
-    { id: 6, name: "Lion", img: "/lion.png" },
-    { id: 7, name: "Zebra", img: "/zebra.png" },
-    { id: 8, name: "Zebra", img: "/zebra.png" },
-    { id: 9, name: "Panda", img: "/panda.png" },
-    { id: 10, name: "Panda", img: "/panda.png" },
-    { id: 11, name: "Bear", img: "/bear.png" },
-    { id: 12, name: "Bear", img: "/bear.png" },
-    { id: 13, name: "Monkey", img: "/monkey.png" },
-    { id: 14, name: "Monkey", img: "/monkey.png" },
-    { id: 15, name: "Wolf", img: "/wolf.png" },
-    { id: 16, name: "Wolf", img: "/wolf.png" },
+    {id: 1, name: "Tiger", img: "/tiger.png"},
+    {id: 2, name: "Tiger", img: "/tiger.png"},
+    {id: 3, name: "Elephant", img: "/elephant.png"},
+    {id: 4, name: "Elephant", img: "/elephant.png"},
+    {id: 5, name: "Lion", img: "/lion.png"},
+    {id: 6, name: "Lion", img: "/lion.png"},
+    {id: 7, name: "Zebra", img: "/zebra.png"},
+    {id: 8, name: "Zebra", img: "/zebra.png"},
+    {id: 9, name: "Panda", img: "/panda.png"},
+    {id: 10, name: "Panda", img: "/panda.png"},
+    {id: 11, name: "Bear", img: "/bear.png"},
+    {id: 12, name: "Bear", img: "/bear.png"},
+    {id: 13, name: "Monkey", img: "/monkey.png"},
+    {id: 14, name: "Monkey", img: "/monkey.png"},
+    {id: 15, name: "Wolf", img: "/wolf.png"},
+    {id: 16, name: "Wolf", img: "/wolf.png"},
 ];
 
 function CardGrid() {
+    function shuffle(arr: CardType[]) {
+        {
+            let i = arr.length,
+                j,
+                temp;
+            while (--i > 0) {
+                j = Math.floor(Math.random() * (i + 1));
+                temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+    shuffle(cardData);
+
     return (
         <div className="grid grid-cols-4 gap-4">
             {cardData.map((card) => (
-                <Card key={card.id} cardName={card.name} img={card.img}/>
+                <Card key={card.id} cardName={card.name} img={card.img} />
             ))}
         </div>
     );
